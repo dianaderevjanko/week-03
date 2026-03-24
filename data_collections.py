5e772d6d1177e4ec52be1c917176544842a8fe76
dianaderevjanko <dianaderevjanko@gmail.com>
2026-03-24 12:40:43 +0200

    Pabeigts un notestēts 1. uzdevums A daļa 1.punkts

--- data_collections.py
+++ data_collections.py
@@ -1,2 +1,10 @@
-print("Sveiki!") #sarakstu un vārdnīcu prakse (izveidne, iterācija, filtrēšana, kombinēšana)
-)
+skaitli = [1, 2, 3, 4, 5, 8, 10, 12]   #veidojam mainīgo skaitli, kurā ir saraksts ar 5 elementiem 
+print(skaitli)  #lai redzētu rezultātu terminālī.
+
+#Metode .append() vienmēr pievieno jaunu elementu saraksta pašās beigās.
+skaitli.append(15)
+print(skaitli)
+
+#Metode .pop() izdara divas lietas: tā izņem pēdējo elementu no saraksta un to "atdod" (tu vari to saglabāt citā mainīgajā vai vienkārši izdzēst).
+skaitli.pop()
+print(skaitli)
